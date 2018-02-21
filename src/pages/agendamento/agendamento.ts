c02e2e0516afcf5f09079838bff3beef035a89d6
Julio César Mello Boaroli <juliocesar__mb@hotmail.com>
2018-02-20 22:29:37 -0300

    Criando formulário de agendamento

--- src/pages/agendamento/agendamento.ts
+++ src/pages/agendamento/agendamento.ts
@@ -10,6 +10,11 @@ export class AgendamentoPage {
   public carro: Carro;
   public precoTotal: number;
 
+  public nome: string;
+  public endereco: string;
+  public data: string = new Date().toISOString();
+  public email: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.carro = this.navParams.get('carro');
     this.precoTotal = this.navParams.get('precoTotal');
@@ -18,6 +23,13 @@ export class AgendamentoPage {
   // Chamado quando a página for carregada e cacheada
   ionViewDidLoad() {
     console.log('ionViewDidLoad AgendamentoPage');
+  }
+
+  agendar() {
+    console.log(this.nome);
+    console.log(this.endereco);
+    console.log(this.data);
+    console.log(this.email);
   }
 
 }
